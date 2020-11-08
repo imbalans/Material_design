a582adb589be1d64ddda8abf1c668328590457b0
Alexander Andreev <imba.yeah@yandex.ru>
2020-11-09 01:48:11 +0300

    hw_1

---
+++ app/src/main/java/com/example/material_design/ColorTheme.kt
@@ -0,0 +1,11 @@
+package com.example.material_design
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ColorTheme: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.color_theme)
+    }
+}
