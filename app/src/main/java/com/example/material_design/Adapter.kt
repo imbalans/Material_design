8dbcf14d2a9e0276568921f0168250d65bf3aa2f
Alexander Andreev <imba.yeah@yandex.ru>
2020-11-26 02:25:00 +0300

    hw_6

--- app/src/main/java/com/example/material_design/Adapter.kt
+++ app/src/main/java/com/example/material_design/Adapter.kt
@@ -5,30 +5,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 
-class Adapter (var context: Context, var arraylist: ArrayList<PhotoChar>) : RecyclerView.Adapter<Adapter.ItemHolder>() {
+class Adapter(
+    var context: Context,
+    var arraylist: ArrayList<PhotoChar>,
+) : RecyclerView.Adapter<Adapter.ItemHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
-        val itemHolder = LayoutInflater.from(parent.context).inflate(R.layout.grid_layout_list_item, parent, false)
+        val itemHolder = LayoutInflater.from(parent.context).inflate(
+            R.layout.grid_layout_list_item,
+            parent, false
+        )
         return ItemHolder(itemHolder)
     }
 
     override fun onBindViewHolder(holder: ItemHolder, position: Int) {
         var photoChar: PhotoChar = arraylist[position]
 
+        holder.icons.setOnLongClickListener {
+            Snackbar.make(it, "Удалить фото?", Snackbar.LENGTH_SHORT)
+                .setAction("Да") {
+                    val newPosition = holder.adapterPosition
+                    arraylist.removeAt(newPosition)
+                    notifyDataSetChanged()
+                }.show()
+            true
+        }
+
         holder.icons.setImageResource(photoChar.iconsChar!!)
-        holder.title.text = photoChar.alphaChar
     }
 
     override fun getItemCount(): Int {
         return arraylist.size
     }
 
-    class ItemHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var icons: ImageView = itemView.findViewById(R.id.icons_image) // listener
-        var title: TextView = itemView.findViewById(R.id.image_text_view)
-
+    class ItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var icons: ImageView = itemView.findViewById(R.id.icons_image)
     }
 }
